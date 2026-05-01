e395b00461da180ddbf95c3a7be9a20abd967ca7
Saanya Mittal <m.saanyak@gmail.com>
2026-05-01 16:42:04 +0530

    Add problem

---
+++ BuyandSell/Easy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int maxProfit(int* prices,int n){
+
+}
+
+int main(){
+    int prices[6]={7,1,5,3,6,4};
+    int n=sizeof(prices)/sizeof(prices[0]);
+    return 0;
+}
